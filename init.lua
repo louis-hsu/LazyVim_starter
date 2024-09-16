07bd0d27039fd4f560495d8c20aec7f91acdb8f4
Louis Hsu <louis.shiu@gmail.com>
2024-09-16 23:14:58 +0800

    <fix> Fix 'markdown-preview' compile error
    Solution found from internet

--- init.lua
+++ init.lua
@@ -1,2 +1,13 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+spec = {
+  {
+    "iamcco/markdown-preview.nvim",
+    cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
+    ft = { 'markdown' },
+    build = function()
+      vim.cmd [[Lazy load markdown-preview.nvim]]
+      vim.fn['mkdp#util#install']()
+    end,
+  },
+}
